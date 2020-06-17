021f78c9cb937bb82cc209a9d506a320fa596709
Akhil <geoakhil@gmail.com>
2020-06-17 15:06:16 +0530

    Updates

---
+++ precache-manifest.d5d6671b2b68784bd2d9ba40029be92a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0e617717c52b8dccb4c3437aa2be1de",
+    "url": "/akhil.one/index.html"
+  },
+  {
+    "revision": "95ea5c2ae10eb35bb877",
+    "url": "/akhil.one/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "4b6b0dfcd62cd42764db",
+    "url": "/akhil.one/static/css/main.307a871c.chunk.css"
+  },
+  {
+    "revision": "95ea5c2ae10eb35bb877",
+    "url": "/akhil.one/static/js/2.448dd0be.chunk.js"
+  },
+  {
+    "revision": "b71cb3cccc73e099d412c24a04bf9105",
+    "url": "/akhil.one/static/js/2.448dd0be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b6b0dfcd62cd42764db",
+    "url": "/akhil.one/static/js/main.ad79d071.chunk.js"
+  },
+  {
+    "revision": "6140dad6a1d556825d95",
+    "url": "/akhil.one/static/js/runtime-main.2be22009.js"
+  },
+  {
+    "revision": "2b8e9a1eaa4b0f32a634f613b9f79c0e",
+    "url": "/akhil.one/static/media/resume.2b8e9a1e.pdf"
+  }
+]);
