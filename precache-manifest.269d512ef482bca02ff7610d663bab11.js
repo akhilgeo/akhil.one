cc30b7f9f13fa5751bfaab35ec224c962590c0a0
Akhil <geoakhil@gmail.com>
2020-06-17 15:17:44 +0530

    Updates

---
+++ precache-manifest.269d512ef482bca02ff7610d663bab11.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f84a68b60614611c11545fcbe8eb6963",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95ea5c2ae10eb35bb877",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "741bc87978b25db85240",
+    "url": "/static/css/main.307a871c.chunk.css"
+  },
+  {
+    "revision": "95ea5c2ae10eb35bb877",
+    "url": "/static/js/2.448dd0be.chunk.js"
+  },
+  {
+    "revision": "b71cb3cccc73e099d412c24a04bf9105",
+    "url": "/static/js/2.448dd0be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "741bc87978b25db85240",
+    "url": "/static/js/main.f4b7d24d.chunk.js"
+  },
+  {
+    "revision": "c88671eb88bf6c28dee9",
+    "url": "/static/js/runtime-main.6a060e8a.js"
+  },
+  {
+    "revision": "2b8e9a1eaa4b0f32a634f613b9f79c0e",
+    "url": "/static/media/resume.2b8e9a1e.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/akhil.one/precache-manifest.d5d6671b2b68784bd2d9ba40029be92a.js"
+  "/precache-manifest.269d512ef482bca02ff7610d663bab11.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/akhil.one/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
