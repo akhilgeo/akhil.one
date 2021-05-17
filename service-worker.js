8f9686e2426cbfaedf536db079c139aa4dbf64ac
akhilgeo <geoakhil@gmail.com>
2021-05-17 09:19:45 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.acf2fb74196782dcef778d849371808a.js"
+  "/precache-manifest.3989c33f81f0972724e0e98d0d0182ad.js"
 );
 
 self.addEventListener('message', (event) => {
